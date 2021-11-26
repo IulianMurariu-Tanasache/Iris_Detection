113f53025d7cb76515ed552d49e95610d817c76c
IulianMurariu-Tanasache <iulian.murariutanasache@gmail.com>
2021-11-26 11:06:46 +0200

    added basic correction

--- main.py
+++ main.py
@@ -1,23 +1,62 @@
-
+from statistics import mean
 
 import cv2
 import numpy
-import numpy as np
 
 
 def getArea(frame, zone):  # zone: zone x, zone y - > stanga sus si zone w, zone h -> lungime, latime
     return frame[zone[1]:zone[1] + zone[3], zone[0]:zone[0] + zone[2]]
 
-class Cercul:
-    def __init__(self):
-        self.centrus = 0
-        self.centrud = 0
-        self.raza = 0
-        print('centrus....', self.centrus)
-        print('centrus....', self.centrud)
-        print('raza', self.raza)
 
-if __name__ == '__main__':
+class Cercul:
+    def __init__(self, cerc, raza):
+        self.centru = cerc
+        self.raza = raza
+
+
+def to_list(c):
+    list_x = [cerc.centru[0] for cerc in c]
+    list_y = [cerc.centru[1] for cerc in c]
+    list_raza = [cerc.raza for cerc in c]
+    return list_x, list_y, list_raza
+
+
+def mean_circle(lists_left):
+    mean_c = Cercul((int(mean(lists_left[0])), int(mean(lists_left[1]))), int(mean(lists_left[2])))
+    return mean_c
+
+
+marja_eroare = 10
+
+
+# TODO: -procentaj la marja
+#       -refacuta media constant pentru a ajusta la miscari
+#       -sa fie incadrat in patratul ochilui?
+
+
+def check_mean(circle, mean_c):
+    centru_x = circle.centru[0]
+    centru_y = circle.centru[1]
+    if not mean_c.centru[0] - marja_eroare <= circle.centru[0] <= mean_c.centru[0] + marja_eroare:
+        centru_x = mean_c.centru[0]
+    if not mean_c.centru[1] - marja_eroare <= circle.centru[1] <= mean_c.centru[1] + marja_eroare:
+        centru_y = mean_c.centru[1]
+    if not mean_c.raza - marja_eroare <= circle.raza <= mean_c.raza + marja_eroare:
+        circle.raza = mean_c.raza
+    circle.centru = (centru_x, centru_y)
+    return circle
+
+
+def main():
+    iris_left = []
+    iris_right = []
+    frames_correction = 10
+    index_left = 0
+    index_right = 0
+
+    mean_circle_left = None
+    mean_circle_right = None
+
     # obiectul video care acceseaza camera
     capture = cv2.VideoCapture(0)
     font = cv2.FONT_HERSHEY_SIMPLEX
@@ -36,13 +75,21 @@ if __name__ == '__main__':
         eyes = []
         roi_color = None
 
+        if index_left == frames_correction:
+            mean_circle_left = mean_circle(to_list(iris_left))
+            index_left += 1
+
+        if index_right == frames_correction:
+            mean_circle_right = mean_circle(to_list(iris_right))
+            index_right += 1
+
         for (x, y, w, h) in faces:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
             roi_gray = gray[y:y + h, x:x + w]
             roi_color = frame[y:y + h, x:x + w]
             eyes = eyes_frames.detectMultiScale(roi_gray, 1.15, 5)
             for (ex, ey, ew, eh) in eyes:
-                if ey + ew <= (y + w) / 2:  # ochii pot fi doar in jumatatea de sus a fetei
+                if ey + ew <= (y + h) / 2:  # ochii pot fi doar in jumatatea de sus a fetei
                     cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
                     otsu_thresh, thresh = cv2.threshold(getArea(roi_gray, (ex, ey, ew, eh)), 0, 255,
                                                         cv2.THRESH_BINARY + cv2.THRESH_OTSU)
@@ -50,29 +97,39 @@ if __name__ == '__main__':
                     # cv2.imshow('oachi', edges)
                     edges = cv2.GaussianBlur(edges, (5, 5), cv2.BORDER_DEFAULT)
                     c = cv2.HoughCircles(image=edges, method=cv2.HOUGH_GRADIENT, dp=2, minDist=ew, param1=otsu_thresh,
-                                         param2=44.5, minRadius=6, maxRadius=13)
+                                         param2=44, minRadius=6, maxRadius=13)
                     # Then mask the pupil from the image and store it's coordinates.
-                    memorie = []
-                    for i in range(30):
-                       memorie.append(Cercul())
-                    if c is not None:
 
+                    if c is not None:
                         for l in c:
                             # OpenCV returns the circles as a list of lists of circles
                             for circle in l:
                                 if type(circle) == numpy.ndarray:
                                     center = (int(x + ex + circle[0]), int(y + ey + circle[1]))
-                                    memorie[l].centrus = center[0]
-                                    memorie[l].centrud = center[1]
                                     radius = int(circle[2])
-                                    memorie[l].raza = radius
-                                    # print(c, l, circle)
-                                    cv2.circle(frame, center, int(radius), (0, 0, 255), thickness=2)
-                                    pupil = (center[0], center[1], radius)
+                                    print(f'Cerc initial: centru: {center} / raza: {radius} / ', end='')
+                                    cerc = Cercul(center, radius)
+                                    if x < cerc.centru[0] < (x + w / 2):
+                                        print('Stanga')
+                                        if index_left < frames_correction:
+                                            iris_left.append(cerc)
+                                            index_left += 1
+                                        else:
+                                            cerc = check_mean(cerc, mean_circle_left)
+                                    elif (x + w / 2) < cerc.centru[0] < x + w:
+                                        print('Dreapta')
+                                        if index_right < frames_correction:
+                                            iris_right.append(cerc)
+                                            index_right += 1
+                                        else:
+                                            cerc = check_mean(cerc, mean_circle_right)
 
+                                    print(f'Cerc corectat: centru: {cerc.centru} / raza: {cerc.raza}')
+                                    print(index_left, index_right, mean_circle_left, mean_circle_right)
+                                    cv2.circle(frame, cerc.centru, cerc.raza, (0, 0, 255), thickness=2)
 
         cv2.putText(frame, 'Calibration: Look straight', (50, 50), font, 1.3, (0, 0, 0), 2, cv2.LINE_AA)
-        #cv2.imwrite(path + 'pillar_text.jpg', im)
+        # cv2.imwrite(path + 'pillar_text.jpg', im)
         cv2.imshow('Eyes detections', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -82,3 +139,7 @@ if __name__ == '__main__':
 
     capture.release()
     cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
